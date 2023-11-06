1a6ea5508913aed1475fe05f501574f1409b8cdc
tensai-NA <aoi080815@gmail.com>
2023-11-06 14:50:45 +0900

    追加

---
+++ db/db-connect.php
@@ -0,0 +1,8 @@
+<?php
+const SERVER = 'mysql214.phy.lolipop.lan';
+const DBNAME = 'LAA1517358-shop';
+const USER = 'LAA1517358';
+const PASS = 'Pass0808';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
