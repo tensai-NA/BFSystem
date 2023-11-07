7612944d197cdda70b8d83ec72a2d975106b722d
makirui <148730277+makirui@users.noreply.github.com>
2023-11-07 10:49:38 +0900

    変更

--- purchase.php
+++ purchase.php
@@ -8,50 +8,58 @@
 </head>
 <body>
     <h2>購入確認</h2>
-    <p>配送先住所<p>
-        <p>
+    <p>配送先住所
             〇〇〇-▽▽▽▽<br>
             〇〇県××市△△町☐☐-〇<br>
             080-〇〇〇〇-☐☐☐☐<br>
             山田 太郎様<br>
+    </p>
+        <p><button type="submit">変更</button></p>
 
-            <button type="submit">変更</button>
-        </p>
-    <p>配送希望日</p>
+    <p>配送希望日
         <select name="day">
             <option value="">指定しない</option>
 
-    <p>希望時間帯</p>
+    <p>
+        希望時間帯
         <select name="time">
             <option value="">指定しない</option>
+    </p>
     
-    <p>ポイント利用</p>
-        <p>
+    <p>
+        ポイント利用<br>
             ご利用可能ポイント：100pt<br>
             ご利用ポイント：<input id="number" type="number" value="0" />
-        </p>
-    <p>決済方法</p>
+    </p>
+
+    <p>
+        決済方法
         <select name="kessai">
             <option value="">クレカ払い</option>
+    </p>
 
-    <p>ご注文内容</p>
-        <p>
+    <p>
+        ご注文内容<br>
             〇〇〇<br>
             カラー：ピンク<br>
             価格：￥0,000<br>
             小計　￥00000
-        </p>
+    </p>
     
     <hr>
-        <p>
-            リピート割　　　　　　　　-￥000
-            商品点数　　　　　　　　　　〇点
-            代金合計　　　　　　　　 ￥00000
-            送料　　　　　　　　　　　 ￥000
-        </p>
+
+    <p>
+            リピート割　　　　　　　　-￥000<br>
+            商品点数　　　　　　　　　　〇点<br>
+            代金合計　　　　　　　　 ￥00000<br>
+            送料　　　　　　　　　　　 ￥000<br>
+    </p>
     <hr>
-    <p>ご注文合計　　　　　　　　￥00000<p>
-        <p>獲得予定ポイント　000pt</p>
+
+    <p>
+        ご注文合計　　　　　　　　￥00000<br>
+        獲得予定ポイント　000pt
+    </p>
 
     <button type="submit">ご注文を確定する</button>
     <a href="cart.php">カートへ戻る</a>
