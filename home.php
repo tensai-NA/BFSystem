16ddd8397cd0eba8d81c4a831bbdef35fc6b6675
tensai-NA <aoi080815@gmail.com>
2023-11-07 09:41:01 +0900

    test

--- home.php
+++ home.php
@@ -11,6 +11,31 @@
         <img src="sozai/cart.png" width="30px" height="30px">
         <p>マイポイント 〇〇pt</p>
         <p>search</p>
+        <script src="https://code.jquery.com/jquery.min.js"></script>
+            <script>
+            $(function() {
+                $(".A").click(function() {
+                    $(".B").toggleClass("C");
+                });
+            });
+            </script>
+            <style>
+            .A{
+                display: inline-block;
+                background: #b6beff;
+                padding: 5px 10px;
+                cursor: pointer;
+            }
+            .B{
+                background: #ffaf74;
+                height: 100px;
+            }
+            .C{
+                display: none;
+            }
+            </style>
+            <div class="A">ボタン</div>
+            <div class="B">これが表示/非表示</div>
         <h2>おすすめ</h2>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 </body>
