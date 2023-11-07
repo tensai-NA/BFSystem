a68751e0f237af65bb2f756eb7792ebd5587c3c3
tensai-NA <aoi080815@gmail.com>
2023-11-07 11:21:46 +0900

    henko

--- home.php
+++ home.php
@@ -8,12 +8,12 @@
     <title>ホーム画面</title>
 </head>
 <body>
-        <img src="sozai/mypage.png" width="30px" height="30px">
-        <img src="sozai/cart.png" width="30px" height="30px">
-        <p>マイポイント 〇〇pt</p>
-        <div class="A">search</div>
+    <img src="sozai/mypage.png" width="30px" height="30px">
+    <img src="sozai/cart.png" width="30px" height="30px">
+    <p>マイポイント 〇〇pt</p>
+    <div class="A">search</div>
         <div class="B">
-            <label for="cate">カテゴリ検索</label>
+            <h4>カテゴリ検索</h4>
                 <input type="radio" name="kate" value="1">
                     <label for="hinmei">商品名</label>
                 <input type="text" id="hinmei"><br>
@@ -25,18 +25,39 @@
                 <input type="radio" name="kate" value="3">
                     <label for="color">カラー</label>
                 <input type="text" id="color"><br>
-
-
+            <h4>金額検索</h4>
+                <label>下限：
+                <select name="drop01">
+                    <option value="0">指定なし</option>
+                    <option value="1000">1000円</option>
+                    <option value="2000">2000円</option>
+                    <option value="3000">3000円</option>
+                    <option value="4000">4000円</option>
+                </select>
+                </label>
+                <label>上限：
+                <select name="drop02">
+                <option value="0">指定なし</option>
+                    <option value="1000">1000円</option>
+                    <option value="2000">2000円</option>
+                    <option value="3000">3000円</option>
+                    <option value="4000">4000円</option>
+                </select>
+                </label>
+            <br>
+            <button>検索</button>
         </div>
-        <script src="https://code.jquery.com/jquery.min.js"></script>
-        <script>
-            $(function() {
-                $(".A").click(function() {
-                    $(".B").toggleClass("C");
-                });
+    <script src="https://code.jquery.com/jquery.min.js"></script>
+    <script>
+        $(function() {
+            $(".A").click(function() {
+                $(".B").toggleClass("C");
             });
-        </script>
+        });
+    </script>
+    <div class="a">
         <h2>おすすめ</h2>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 </body>
 </html>
