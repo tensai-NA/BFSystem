743d416917bce8255bb984f21716ab9b78b0ca5b
mizo0122 <148730198+mizo0122@users.noreply.github.com>
2023-11-07 10:32:16 +0900

    変更

--- logout.php
+++ logout.php
@@ -1,1 +1,18 @@
 <!--　担当：溝口　14.ログアウト確認画面-->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h4>ログアウトしますか？</h4>
+    ※　再度ログインするためにはメール<br>
+    アドレスとパスワードが必要です<br>
+    <button type="submit" name="logout">ログアウトする➝</button><br>
+    
+    
+        <button type="submit" name="cancel">キャンセル➝</button>
+</body>
+</html>
