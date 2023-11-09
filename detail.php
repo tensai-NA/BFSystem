83afbfee5c226ed32329f54af1556baf4bc5e9b8
Azure1211 <ruri.hr1@icloud.com>
2023-11-09 10:53:10 +0900

    detail.php main.css

--- detail.php
+++ detail.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
     <link rel="stylesheet" href="css/main.css">
     <title>Document</title>
@@ -12,28 +11,30 @@
 <body>
     <div class="head">
     
-        <a href=""><i class="fas fa-long-arrow-alt-left"></i></a> <!--　矢印下線消す　改行をなくす--> 
-        <a href="cart.php"><i class="fas fa-shopping-cart"></i></a>
-        <a href="home.php"><i class="fas fa-home"></i></a>
-    </div>
+        <p class="back">
+            <a href="search.php"><i class="fas fa-long-arrow-alt-left fa-3x" ></i></a>
+        </p>
+
+        <p class="headitems">
+            <a href="cart.php"><i class="fas fa-shopping-cart fa-3x" ></i></a>
+            <a href="home.php"><i class="fas fa-home fa-3x"></i></a>
+        </p>
+
+    </div><hr>
 
     <div class="main">
-        <!-- 
-            li {
-            list-style: none;
-             }
-        -->
-        <img src="" alt="image1" width="" hight="">
+        <img src="sozai/mypage.png"  width="70%"alt="image1">
         <ul>
-            <li><img src="" width="" height="" alt="image2"></li>
-            <li><img src="" width="" height="" alt="image3"></li>
-            <li><img src="" width="" height="" alt="image4"></li>
-            <li><img src="" width="" height="" alt="image5"></li>
+            <li><img src="sozai/mypage.png" width="90%" alt="image2"></li>
+            <li><img src="sozai/mypage.png" width="90%" alt="image3"></li>
+            <li><img src="sozai/mypage.png" width="90%" alt="image4"></li>
+            <li><img src="sozai/mypage.png" width="90%" alt="image5"></li>
         </ul>
         <h3>商品名</h3>
         <h3>価格</h3>
-        <button onclick="loction.href='cart.php'">カートに入れる</button>
+        <button  onclick="loction.href='cart.php'">カートに入れる</button>
     </div>
+
     <div class="notlogin">
         <p>カートに追加するにはログインしてください</p><!--赤字-->
         <p>ログインは <a href="login.php">こちら</a></p>
