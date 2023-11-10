65a3baa56cba003e0d08f1e1cebead4d94b256cc
gameo00 <148730259+gameo00@users.noreply.github.com>
2023-11-10 12:28:42 +0900

    henkou

--- login.php
+++ login.php
@@ -1,26 +1,40 @@
 <?php session_start(); ?>
 <?php require 'kyotu/db-connect.php'; ?>
-<?php 
+<?php
+$msgMail = '';
+$msgPass = '';
+if(isset($_POST['login'])){
     unset($_SESSION['customer']);
-    $ps = password_hash($_POST['password'],PASSWORD_DEFAULT);
     $pdo=new PDO($connect,USER,PASS);
-    $sql=$pdo->prepare('select * from customer where login=?');
-    $sql->execute([$_POST['login']]);
-
-    foreach($sql as $row){
-        if(password_verify($_POST['password'],$row['password']) == true){
-        $_SESSION['customer']=[
-            'id'=>$row['id'],'name'=>$row['name'],
-            'address'=>$row['address'],'login'=>$row['login'],
-            'password'=>$ps];
-        }else{
-            echo 'ログイン名またはパスワードが違います。';
+    $sql=$pdo->prepare('select * from User where mail=?');
+    $sql->execute([$_POST['name']]);
+    $data = $sql->fetchAll();
+    if( empty($data) ){
+        $msgMail = "メールアドレスが登録されていません";
+    }else{
+        foreach($data as $row){
+            if(password_verify($_POST['password'],$row['password']) == true){
+            $_SESSION['customer']=[
+                'user_id'=>$row['user_id'],
+                'user_sei'=>$row['user_sei'],
+                'user_mei'=>$row['user_mei'],
+                'mail'=>$row['mail'],
+                'password'=>$row['password'],
+                'postnum'=>$row['postnum'],
+                'point'=>$row['point'],
+                'address'=>$row['address'],
+                'password'=>$ps];
+            }else{
+                $msgPass = 'パスワードが違います。';
+            }
         }
     }
     
     if(isset($_SESSION['customer'])){
         echo 'いらっしゃいませ、',$_SESSION['customer']['name'],'さん。';
+        //header('location: home.php');
     }
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -33,10 +47,10 @@
 <body>
     <form action="login.php" method="post">
      <p>メールアドレス<br>
-     <input type="text" name="name"></p>
+     <input type="text" name="name"><br><?= $msgMail ?></p>
      <p>パスワード<br>
-     <input type="text" name="pasu"><br>
-        <button type="submit">ログイン</button>
+     <input type="text" name="password"><br><?= $msgPass ?></p>
+        <button type="submit" name="login" value="send">ログイン</button>
         <button type="submit">キャンセル</button>
         <p><a href="toroku.php">新規登録はこちら</a></p>
         <p><a href="password.php">パスワードを忘れた方はこちら</a></p>
