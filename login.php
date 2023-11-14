b56e30d8f62c4fea7106fcebdaebbf1848cda8db
Azure1211 <ruri.hr1@icloud.com>
2023-11-14 12:34:15 +0900

    a

--- login.php
+++ login.php
@@ -43,18 +43,44 @@ if(isset($_POST['login'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <title>ログイン</title>
 </head>
 <body>
+    <div class="m-4 has-text-centered ">
+        <h1 class="title is-4"> ログイン</h1>
+
     <form action="login.php" method="post">
-     <p>メールアドレス<br>
-     <input type="text" name="name"><br><?= $msgMail ?></p>
-     <p>パスワード<br>
-     <input type="text" name="password"><br><?= $msgPass ?></p>
+
+    <div class=" box has-background-light m-6">
+        <div class="control m-1">
+        <label class="label">メールアドレス</label>
+        <div class="field has-addons-fullwidth has-addons-centered">
+            <p class="control has-icons-left">
+                <input class="input is-success  is-normal is-focused "type="email" name="meru"  placeholder="メールアドレス"><br><?= $msgMail ?></p>
+             <span class="icon is-small is-left">
+                <i class="fas fa-mail-bulk"></i>
+                </span>
+                </p>
+    </div></div>
+
+    <div class="control m-1">
+        <label class="label">パスワード</label>
+        <div class="field has-addons-fullwidth has-addons-centered">
+            <p class="control has-icons-left">
+     <input type="text"  class="input  is-normal is-focused "name="password"><?= $msgPass ?>
+     <span class="icon is-small is-left">
+        <i class="fas fa-key"></i>
+    </span>
+        </p>
+            </div></div>
+
         <button type="submit" name="login" value="send">ログイン</button>
         <button type="submit">キャンセル</button>
         <p><a href="toroku.php">新規登録はこちら</a></p>
         <p><a href="password.php">パスワードを忘れた方はこちら</a></p>
     </form>
+    </div>
+    </div>
 </body>
 </html>
