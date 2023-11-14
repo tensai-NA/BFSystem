2cf5bf759af04c75c1d0b1eea35974e5c284f6e5
Daisuke2525 <148730269+Daisuke2525@users.noreply.github.com>
2023-11-14 11:13:19 +0900

    a

--- mypage.php
+++ mypage.php
@@ -1,4 +1,7 @@
 <!--　担当：川崎　10.マイページ-->
+
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -8,32 +11,30 @@
 </head>
 <body>
 <h2>マイページ</h2>
-<p>マイページを閲覧するにはログインしてください</p>
-<p><a href="login.php">ログインはこちら</a></p>
-<P><img alt="sozai" src="sozai/home.jpg"></p>
-<p>○○○○様</p>
+<font color="red">マイページを閲覧するにはログインしてください</font>
+<br>ログインは<a href="login.php">こちら</a></br>
+<br>○○○○様　　　　　　　　　<img src="sozai/home.png" width="30" height="30"></p>
 <p>ポイント</p>
 <hr>
-<p>利用可能ポイント: 0pt</p>
+<p>利用可能ポイント:　　　 0pt</p>
 <p>最近の購入履歴</p>
-<hr>
-<h2>2023/00/00</h2>
     <hr>
-    <p>○○</p>
-    <p>カラー:ピンク</p>
-    <p>価格:￥0,000</p>
+    <p>○○<br>
+    カラー:ピンク<br>
+    価格:￥0,000</p>
 
-    <p>○○</p>
-    <p>カラー:オレンジ</p>
-    <p>価格:￥0,000</p>
+    <p>○○<br>
+    カラー:オレンジ<br>
+    価格:￥0,000</p>
 
-    <p>○○</p>
-    <p>カラー:イエロー</p>
-    <p>価格:￥0,000</p>
-    <p><a href="mypage.php">購入履歴一覧へ→</a></p>
+    <p>○○<br>
+    カラー:イエロー<br>
+    価格:￥0,000</p>
+    　　　　　　　　　　<a href="history.php">購入履歴一覧へ→</a></p>
 
-    <p><a href="logout.php">ログアウト</a></p>
-    <p><a href="toroku.php">登録更新更新</a></p>
+
+<p><a href="logout.php">ログアウト</a><br>
+<a href="update.php">登録情報更新</a></p>
 </body>
 </html>
 
