4dea3928d4658944df39b4fd42324bad17b637db
Azure1211 <ruri.hr1@icloud.com>
2023-11-14 12:06:53 +0900

    Update toroku.php

--- toroku.php
+++ toroku.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link rel="stylesheet" href="css/main.css">
     <title>会員登録</title>
 </head>
 <body>
@@ -25,40 +26,88 @@
             }
         
         echo '<form action="toroku-output.php" method="post">';
-        echo '<table>';
+        echo '<div class="m-4 has-text-centered ">
+                 <h1 class="title is-4"> 会員情報登録</h1>
+                  <div class="box has-background-light m-6">
+                     <div class="field">';
 
-        echo '<tr><td>お名前</td><td>';
-        echo '<input type="text" name="sei" v-model="sei" value="',$sei,'"/>','<input type="text" name="mei" v-model="mei" value="',$mei,'"/>';
-        echo '</td></tr>';
+        echo ' <div class="control m-1">
+                <label class="label"> お名前 </label>
+                <div class="field  has-addons has-addons-centered "> ';
+        echo '<input type="text" class="input  is-normal is-focused m-1" name="sei" v-model="sei"value="',$sei,'"/>',
+                '<input type="text" class="input  is-normal is-focused m-1" name="mei" v-model="mei"value="',$mei,'"/>';
+        echo '</div><div>';
 
-        echo '<tr><td>メールアドレス</td><td>';
-        echo '<input type="text" name="mail" v-model="email" value="',$mail,'"/>';
-        echo '<p v-if="isInValidEmail" class="has-text-danger">Eメールアドレスの形式で入力してください</p>';
-        echo '</td></tr>';
 
-        echo '<tr><td>パスワード</td><td>';
-        echo '<input type="password" name="password" v-model="pass1" value="',$password,'"/>';
-        echo '</td></tr>';
+        echo '  <div class="control m-1">
+                 <label class="label">メールアドレス</label>';
+        echo '   <div class="field has-addons-fullwidth has-addons-centered">
+                     <p class="control has-icons-left">
+                         <input type="text"  class="input  is-primary  is-normal is-focused " name="mail" v-model="email" value="',$mail,'"/>
+                         <span class="icon is-small is-left">
+                             <i class="fas fa-mail-bulk"></i>
+                          </span>
+                    </p>';
 
-        echo '<tr><td>パスワード確認</td><td>';
-        echo '<input type="password" name="password2" v-model="pass2" value="',$password2,'"/>';
+        echo    '<p v-if="isInValidEmail" class="has-text-danger">Eメールアドレスの形式で入力してください</p>';
+        echo '</div></div>';
+
+
+        echo '  <div class="control m-1">
+        <label class="label">パスワード</label>';
+        echo '   <div class="field has-addons-fullwidth has-addons-centered">
+                    <p class="control has-icons-left">
+                     <input type="password"  class="input  is-normal is-focused " name="password" v-model="pass1" value="',$password,'"/>
+                     <span class="icon is-small is-left">
+                     <i class="fas fa-key"></i>
+                 </span>
+             </p>';
+        echo '</div></div>';
+
+
+        echo ' <div class="control m-1">
+                <label class="label">パスワード確認</label>';
+        echo '  <div class="field has-addons-fullwidth has-addons-centered">
+                    <p class="control has-icons-left">
+                     <input type="password"  class="input  is-primary is-normal is-focused " name="password2" v-model="pass2" value="',$password2,'" />
+                     <span class="icon is-small is-left">
+                     <i class="fas fa-key"></i>
+                 </span>
+             </p>';
         echo '<p v-if="isInValidPass" class="has-text-danger">パスワードがちがいます。';
-        echo '</td></tr>';
+        echo '</div></div>';
 
-        echo '<tr><td>郵便番号</td><td>';
-        echo '<input type="text" name="postnum" v-model="postnum" value="',$postnum,'"/>';
+
+
+        echo '<div class="control m-1">
+                 <label class="label">郵便番号</label>';
+        echo ' <div class="field has-addons-fullwidth has-addons-centered">
+                 <p class="control has-icons-left">
+                    <input type="text" class="input  is-normal is-focused " name="postnum" v-model="postnum" value="',$postnum,'"/>
+                    <span class="icon is-small is-left">
+                    <i class="has-text-weight-bold">〒</i>
+                 </span>
+             </p>';
         echo '<p v-if="isInValidPost" class="has-text-danger">郵便番号は7桁の数字で入力してください。';
-        echo '</td></tr>';
+        echo '</div></div>';
 
-        echo '<tr><td>住所</td><td>';
-        echo '<input type="text" name="address" value="',$address,'">';
-        echo '</td></tr>';
+
+        echo ' <div class="control m-1">
+                 <label class="label">住所</label>';
+        echo ' <div class="field has-addons-fullwidth has-addons-centered">
+                 <p class="control has-icons-left">
+                    <input type="text" class="input  is-primary is-normal is-focused " name="address" value="',$address,'">
+                    <span class="icon is-small is-left">
+                    <i class="fas fa-house-user"></i>
+                  </span>
+                    </p>';
+        echo '</div></div>';
 
         echo '</table>';
-        echo '<input type="submit" value="確定">';
+        echo '<input type="submit" class="button is-danger m-3" value="確定">';
         echo '</form>';
 
-        echo '<button onclick="location.href=\'login.php\'">キャンセル</button>';
+        echo '<button onclick="location.href=\'login.php\'" class="button is-info m-3">キャンセル</button>';
         echo '<p><a href="login.php">アカウントをお持ちの方はこちら</a></p>'
         ?>
     </div>
