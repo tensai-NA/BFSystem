f5b4c73d599a019114e9f3fd25792f545bb48f44
makirui <148730277+makirui@users.noreply.github.com>
2023-11-24 12:24:20 +0900

    henko

--- detail.php
+++ detail.php
@@ -31,14 +31,29 @@
         <ul>
           
         </ul>
+        <?php
+            if(isset($_SESSION['customer'])){
+                $id = $_SESSION['customer']['user_id']; //ログイン済みの処理
+                $pdo=new PDO($connect,USER,PASS);
+                $sql=$pdo->query("select Shohin.shohin_id,Shohin.shohin_img,Shohin.shohin_mei,Shohin.price
+                                    from Shohin
+                                    where Shohin.shohin_id = Cart.shohin_id
+                                    and Cart.user_id = '".$id."'");
+            }
+        
         <h3>商品名</h3>
         <h3>価格</h3>
         <button  onclick="loction.href='cart.php'">カートに入れる</button>
     </div>
 
     <div class="notlogin">
-        <p>カートに追加するにはログインしてください</p>
-        <p>ログインは <a href="login.php">こちら</a></p>
+        <?php
+            if(!isset($_SESSION['customer'])){
+                echo '<p>カートを閲覧するにはログインしてください</p>';
+                echo '<p><a href="login.php">ログインはこちら</a></p>';
+            exit();
+            }
+    ?>
         
     </div>
 </body>
