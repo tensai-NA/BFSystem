64d101100ae32bb2a242642dba82f008c69086c1
tensai-NA <aoi080815@gmail.com>
2023-12-01 13:04:34 +0900

    やりたくないーー

--- kanri/kanriadd.php
+++ kanri/kanriadd.php
@@ -14,17 +14,29 @@ if(isset($_POST['toroku'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <title>管理者登録</title>
 </head>
 <body>
-    <p>管理者登録</p>
+<div class="m-4 has-text-centered ">
+    <h1 class="title is-4">管理者登録</h1>
+    <div class="box has-background-light m-6">
+        <div class="field">
+        <div class="control m-1">
     <form action="kanriadd.php" method="post">
-        <label>管理者名</label>
-        <p><input type="text" name="name"></p>
-        <label>パスワード</label>
-        <p><input type="password" name="pass"></p>
-        <button name="toroku">登録</button>
-    </form>
-    <button onclick="location.href='login.php'">ログインへ</button>
+        <div class="field has-addons-fullwidth has-addons-centered">
+            <label class="label">管理者名</label>
+            <input type="text" class="input is-normal is-focused m-i" name="name">
+        </div></div>
+        <div class="control m-1">
+        <div class="field has-addons-fullwidth has-addons-centered">
+            <label class="label">パスワード</label>
+            <input type="password" class="input is-primary is-focused m-i" name="pass">
+        </div></div>
+        <button name="toroku" class="button is-danger m-5" >登録</button>
+        </form>
+        <button type="button" onclick="location.href='login.php'" class="button is-info m-5">ログイン</button>
+        </div></div>
+</div>
 </body>
 </html>
--- kanri/shohinadd.php
+++ kanri/shohinadd.php
@@ -27,26 +27,39 @@ if(isset($_POST['add'])){   // 追加ボタンが押された処理
 <body>
     <p>商品管理</p>
     <?php
-    $sql=$pdo->prepare("select * from Shohin");
+    $per_page = 10;
+    $page = isset($_GET['page']) ? $_GET['page'] : 1; // 現在のページ番号
+    $start_index = ($page - 1) * $per_page;
+
+    $sql=$pdo->query("select * from Shohin");    
+    $total_results = $sql->rowCount(); //総データ数
+    $total_pages = ceil($total_results / $per_page);
+
+    $sql = $pdo->prepare("SELECT * FROM Shohin LIMIT :start_index, :per_page");
+    $sql->bindValue(':start_index', $start_index, PDO::PARAM_INT);
+    $sql->bindValue(':per_page', $per_page, PDO::PARAM_INT);
     $sql->execute();
     ?>
     <table>
         <tr><th>商品名</th><th>金額</th><th>色</th><th>ブランド</th><th>カテゴリ</th><th>画像ファイル名</th><th>操作</th></tr>
         <tr>
+            <div class="field">
             <form action="" method="post">
-                <td><input type="text" name="name" require></td> <!--商品名-->
-                <td><input type="number" name="price" require></td> <!--金額-->
-                <td><input type="text" name="color" require></td> <!--色-->
-                <td><input type="text" name="brand" require></td> <!--ブランド-->                
-                <td><input type="text" name="cate" require></td> <!--カテゴリ-->
-                <td><input type="text" name="img" require></td> <!--画像-->
+                <td><input type="text" class="input is-small" name="name" require></td> <!--商品名-->
+                <td><input type="number" class="input is-small" name="price" require></td> <!--金額-->
+                <td><input type="number" class="input is-small" name="color" require></td> <!--色-->
+                <td><input type="number" class="input is-small" name="brand" require></td> <!--ブランド-->                
+                <td><input type="number" class="input is-small" name="cate" require></td> <!--カテゴリ-->
+                <td><input type="text" class="input is-small" name="img" require></td> <!--画像-->
                 <td><button type="submit" name="add">追加</button></td>
             </form>
+            </div>
         </tr>
         <?php
             foreach($sql as $row){
         ?>
         <tr>
+        <div class="field">
             <form action="" method="post">
             <input type="hidden" name="id" value="<?= $row['shohin_id'] ?>">
             <td><input type="text" name="name" value="<?=  $row['shohin_mei'] ?>" require></td>
@@ -58,55 +71,59 @@ if(isset($_POST['add'])){   // 追加ボタンが押された処理
             <td><button type="submit" name="update">更新</button>
                 <button type="submit" name="del">削除</button></td>
             </form>
+        </div>
         </tr>
     </table>
-
-
         <?php
             }
-
-            echo '<div class="columns">';
-                echo '<div class="column">';
+            echo '<nav class="pagination" role="navigation" aria-label="pagination">
+            <ul class="pagination-list">';
+    
+                for ($i = 1; $i <= $total_pages; $i++) {
+                    if ($i == $page) {
+                        echo "<li><a class='pagination-link is-current'>$i</a></li>";
+                    } else {
+                        echo "<li><a class='pagination-link' href='?page=$i'>$i</a><li>";
+                    }
+                }
+            ?>
+        </ul>
+    </nav>
+    
+            <?php
                     echo '<p>カラー</p>';
                     echo '<table>';
-                    $sql=$pdo->query("select * from Color");
-                    foreach($sql as $row){
+                    $color=$pdo->query("select * from Color");
+                    foreach($color as $row){
                         echo '<tr>';
                         echo '<td>',$row['color_code'],'</td>';
                         echo '<td>',$row['color_mei'],'</td>';
                         echo '</tr>';
                     }
                     echo '</table>';
-                echo '</div>';
 
-                echo '<div class="column">';
                     echo '<p>ブランド</p>';
                     echo '<table>';
-                    $sql=$pdo->query("select * from Brand");
-                    foreach($sql as $row){
+                    $brand=$pdo->query("select * from Brand");
+                    foreach($brand as $row){
                         echo '<tr>';
                         echo '<td>',$row['brand_code'],'</td>';
                         echo '<td>',$row['brand_mei'],'</td>';
                         echo '</tr>';
                     }
                     echo '</table>';
-                echo '</div>';
 
-                echo '<div class="column">';
                     echo '<p>カテゴリー</p>';
                     echo '<table>';
-                    $sql=$pdo->query("select * from Categori");
-                    foreach($sql as $row){
+                    $cate=$pdo->query("select * from Categori");
+                    foreach($cate as $row){
                         echo '<tr>';
                         echo '<td>',$row['cate_code'],'</td>';
                         echo '<td>',$row['cate_mei'],'</td>';
                         echo '</tr>';
                     }
                     echo '</table>';
-                echo '</div>';
-            echo '</div>';
 
         ?>
-    </table>
 </body>
 </html>
